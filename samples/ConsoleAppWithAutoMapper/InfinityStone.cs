227223666e2dd3e7d08eaaf0046615bb35a46ea0
Jonathan Madison <jdmadison@gmail.com>
2019-10-02 22:45:38 -0500

    added a bit of setup to help get started with the core feature

---
+++ samples/ConsoleAppWithAutoMapper/InfinityStone.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppWithAutoMapper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InfinityStone
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ samples/ConsoleAppWithAutoMapper/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleAppWithAutoMapper
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using PaginableCollections;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new Profiles());
+            });
+
+            IMapper mapper = config.CreateMapper();
+
+            var infitiyStones = new List<InfinityStone>
+            {
+                new InfinityStone { Name = "Space", Color = "Blue" },
+                new InfinityStone { Name = "Reality", Color = "Red" },
+                new InfinityStone { Name = "Power", Color = "Purple" },
+                new InfinityStone { Name = "Mind", Color = "Yellow" },
+                new InfinityStone { Name = "Time", Color = "Green" },
+                new InfinityStone { Name = "Soul", Color = "Orange" }
+            };
+
+            var infinityStoneDtos = mapper.Map<List<InfinityStoneDto>>(infitiyStones);
+
+            infinityStoneDtos
+                .ForEach(x => Console.WriteLine($"The {x.Name} is {x.Color}"));
+
+            Console.ReadLine();
+        }
+    }
+}
